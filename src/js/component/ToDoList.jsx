0d12c9fb88585128d2fb93b4553e48098fed4f34
Tgodwin94 <tyler.godwin94@gmail.com>
2024-10-08 22:32:04 +0000

    TylerGodwin-ToDoList-With-React-And-Fetch

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -1,48 +1,120 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const ToDoList = () => {
-  const [inputValue, setInputValue] = useState("");
-  const [todos, setTodos] = useState([]);
 
-  const handleKeyDown = (e) => {
-    if (e.key === "Enter" && inputValue.trim() !== "") {
-      setTodos([...todos, inputValue]);
-      setInputValue("");
-    }
-  };
+	const [newTodo, setNewTodo] = useState("");
+	const [list, setList] = useState([])
 
-  const handleDelete = (index) => {
-    setTodos(todos.filter((t, currentIndex) => currentIndex !== index));
-  };
 
-  return (
-    <div className="container">
-      <h1>To-Do List</h1>
-      <ul className="todoList">
-        {" "}
-        <li>
-          <input
-            type="text"
-            onChange={(e) => setInputValue(e.target.value)}
-            value={inputValue}
-            onKeyDown={handleKeyDown}
-            placeholder="Enter a to-do task"
-          ></input>
-        </li>
-        {todos.map((item, index) => (
-          <li key={index}>
-            {item}{" "}
-            <i
-              className="fa-solid fa-trash-can"
-              onClick={() => handleDelete(index)}
-            ></i>
-          </li>
-        ))}
-      </ul>
-      <div className="taskCount">{todos.length} tasks</div>
-    </div>
-  );
-};
+
+	const handleChange = (event) => {
+
+		setNewTodo(event.target.value)
+
+	}
+	
+
+	const createUser = () => {
+		fetch('https://playground.4geeks.com/todo/users/Tgodwin94', {
+			method: "POST",
+
+		})
+			.then(resp => {
+			
+				return resp.json(); 
+			})
+			.then(data => {
+				
+				console.log(data);
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	}
+	const getList = async () => {
+		try {
+			const response = await fetch('https://playground.4geeks.com/todo/users/Tgodwin94')
+			const data = await response.json()
+			if (response.status == 404) {
+				createUser()
+
+			}
+			if (response.ok) {
+				setList(data.todos)
+			}
+		} catch (error) { }
+	}
+
+	const deleteUser = async (id) => {
+
+		try {
+			const response = await fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
+				method: "DELETE",
+				headers: { "Content-Type": "application/json" },
+			})
+
+			getList()
+
+		}
+		catch (error) {
+			console.log(error)
+
+		}
+	}
+
+	
+	const addNewTask = () => {
+		fetch('https://playground.4geeks.com/todo/todos/Tgodwin94', {
+			method: "POST",
+			body: JSON.stringify({
+				"label": newTodo,
+				"is_done": false
+			}),
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(resp => {
+				if (resp.ok) {
+					getList()
+				}
+			})
+
+			.catch(error => {
+				console.log(error);
+			});
+	}
+
+	useEffect(() => {
+		getList()
+	}, [])
+
+
+	return (
+		<div className="text-center">
+			<h1 className="text-center mt-5">
+				To-Do List
+			</h1>
+			<div className="postTodo">
+				<input className="typeTodo" type="text" value={newTodo} onChange={handleChange} />
+				<button type="button" class="btn btn-success" onClick={addNewTask}  >
+					Add a to-do
+				</button>
+			</div>
+			<p><b>New To-Do:</b> {newTodo} </p>
+			<ul>
+				{list.map((item, index) => {
+					return (
+						<li key={index}>
+							{item.label}<button type="button" class="btn btn-danger" onClick={() => deleteUser(item.id)}> Delete </button>
+
+						</li>
+            
+					)
+				})}
+			</ul>
+		</div>
+	)
+}
 
 export default ToDoList;
